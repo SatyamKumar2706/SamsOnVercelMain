315b6e9e6848e46941e001223e506ab1761cdcc2
Sk Meher <satyabanmeher006@gmail.com>
2024-09-27 00:15:48 +0530

    ...

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Set up CORS configuration
 app.use(cors({
-    origin: process.env.FRONTEND_URL, // Should be without trailing slash
+    origin:"https://sams-frontend-on-versel--mocha.vercel.app/",
     credentials: true // Allow credentials to be included in requests
 }));
 
