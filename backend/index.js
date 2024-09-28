41e8f8888947d4e06ff0c57ef0db06c2da68b8a4
Sk Meher <satyabanmeher006@gmail.com>
2024-09-29 00:12:51 +0530

    ...

--- backend/index.js
+++ backend/index.js
@@ -21,17 +21,8 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', corsOptions.origin);
-    res.header('Access-Control-Allow-Methods', corsOptions.methods.join(', '));
-    res.header('Access-Control-Allow-Headers', corsOptions.allowedHeaders.join(', '));
-    res.header('Access-Control-Allow-Credentials', corsOptions.credentials);
-    
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(corsOptions.optionsSuccessStatus);
-    }
-    next();
-});
+// Enable pre-flight requests for all routes
+app.options('*', cors(corsOptions));
 
 app.use(express.json({ limit: '10mb' }));
 
